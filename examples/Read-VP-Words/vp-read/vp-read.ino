622d2de5cba8c3166ab40a2d39149a6a2db56c85
Paul Mendum <satspares@gmail.com>
2023-10-01 17:49:02 +0100

    update example

--- examples/Read-VP-Words/vp-read/vp-read.ino
+++ examples/Read-VP-Words/vp-read/vp-read.ino
@@ -61,11 +61,13 @@ DWIN hmi(DGUS_SERIAL, DGUS_BAUD, false); // the false do not init the port yet
 #define DGUS_SERIAL Serial2
 DWIN hmi(DGUS_SERIAL, 16, 17, DGUS_BAUD);
 
+
 // If Using Arduino Uno
 #else
 DWIN hmi(2, 3, DGUS_BAUD);
 #endif
-
+// include the helpers
+#include "DWIN_Arduino_Helpers.hpp"
 // Event Occurs when response comes from HMI
 void onHMIEvent(String address, int lastByte, String message, String response);
 unsigned long getWordReply(String response, byte bytesBack);
@@ -135,40 +137,4 @@ void onHMIEvent(String address, int lastByte, String message, String response)
   }
 }
 
-
-/**
- * 
- * Get the last 2 bytes returned from hmi for our use if bytesBack = 0
- * If bytesBack = 2 get the 2 previous bytes etc. 
- */
-unsigned long getWordReply(String response, byte bytesBack){
-  int str_len;
-  char *ptr = NULL;
-  str_len = response.length() + 1; 
-  char *strings[str_len];  // we dont need this much memory
-  char response_array[str_len];
-  response.toCharArray(response_array,str_len);
-  ptr = strtok(response_array, " " );
-
-  byte index = 0;
-  while(ptr != NULL)
-  {
-      strings[index] = ptr;
-      index++;
-      ptr = strtok(NULL, " ");
-  }
-//  for(int i = 0; i < index; i++)
-//  {
-//   /Serial.println(strings[i]);
-//  }
-  index --;
-  index = index - bytesBack;
-  return((unsigned long) strtol(strings[index-1], NULL, 16) << 8) + (unsigned long) strtol(strings[index], NULL, 16);
-}
-
-// Read word from VP address
-uint16_t readVP(uint16_t vpAddress)
-{
-    return (hmi.readVPByte(vpAddress,1) << 8) + hmi.readVPByte(vpAddress) ;
-}
 
